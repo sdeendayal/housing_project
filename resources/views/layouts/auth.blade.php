ad30f2c9c1c9122432f0a9b2ffb27f7d5e2df4e1
Deendayal Singh <85936950+sdeendayal@users.noreply.github.com>
2026-05-21 16:45:51 +0530

    Update Nitish changes

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -143,6 +143,13 @@
             font-variation-settings: 'FILL' 0, 'wght' 400, 'GRAD' 0, 'opsz' 24;
         }
     </style>
+    <!-- Bootstrap -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+<!-- Google Font -->
+<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800&display=swap"
+   rel="stylesheet">
+<!-- Icons -->
+<link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" rel="stylesheet" />
 </head>
 
 <body class="bg-bg-subtle text-on-background font-body-md flex flex-col min-h-screen">
--- resources/views/mmsay/citizenLogin.blade.php
+++ resources/views/mmsay/citizenLogin.blade.php
@@ -1,10 +1,4 @@
-<!-- Bootstrap -->
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-<!-- Google Font -->
-<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800&display=swap"
-   rel="stylesheet">
-<!-- Icons -->
-<link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" rel="stylesheet" />
+
 @extends('layouts.auth')
 @section('title', 'MMSAY Login')
 @section('content')
@@ -399,12 +393,7 @@
                      arrow_forward
                      </span>
                      </button>
-                  </form>
-                  <!-- FOOTER -->
-                  <!-- <div class="footer-text">
-                     © 2026 Haryana Housing Department • All Rights Reserved
-                     
-                     </div> -->
+                  </form>                 
                </div>
             </div>
          </div>
