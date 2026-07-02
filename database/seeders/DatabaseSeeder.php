994bd73357d7bfeccfc6c1e421f13855051d55da
AnandKamboz <anandkamboj01@gmail.com>
2026-07-02 10:04:19 +0530

    feat: add OwnerMasterSeeder and corresponding CSV for owner data import

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -30,6 +30,7 @@ class DatabaseSeeder extends Seeder
             PhysicalPossessionUserSeeder::class,
             LedgerSeeder::class,
             InstallmentDueSeeder::class,
+            OwnerMasterSeeder::class,
             MMGAYUserSeeder::class,
         ]);
 
