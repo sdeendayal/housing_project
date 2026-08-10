14d64aeb721eee9c88e2695edaa745c5a6ec2571
AnandKamboz <anandkamboj01@gmail.com>
2026-08-10 13:13:18 +0530

    feat: add seeder to import AllEwsData544 from Excel via XML streaming

--- database/seeders/AllEwsData544Seeder.php
+++ database/seeders/AllEwsData544Seeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Str;
 
 class AllEwsData544Seeder extends Seeder
