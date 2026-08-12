070d3f0fe20b7afa46b7c27f327efab2a0a73ac7
AnandKamboz <anandkamboj01@gmail.com>
2026-08-12 13:07:50 +0530

    feat: update seeders to skip empty headers and 'id' field during data insertion

--- database/seeders/PanipatEligibleDrawListSeeder.php
+++ database/seeders/PanipatEligibleDrawListSeeder.php
@@ -80,7 +80,7 @@ class PanipatEligibleDrawListSeeder extends Seeder
 
             $rowInsert = [];
             foreach ($header as $h) {
-                if (empty($h)) {
+                if (empty($h) || strcasecmp($h, 'id') === 0) {
                     continue;
                 }
                 
